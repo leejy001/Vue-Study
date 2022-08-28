f19a4b9fd42a04d23671a4484a9200705884df6f
leejy001 <greatness007@naver.com>
2022-08-28 19:12:20 +0900

    뷰 EventBus (틱택토)

---
+++ 7.틱택토_EventBus/EventBus.js
@@ -0,0 +1,5 @@
+// EventBus 사용하면 root 컴포넌트에서 모든 이벤트 처리 가능
+
+import Vue from "vue";
+
+export default new Vue();
---
+++ 7.틱택토_EventBus/TdComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+    import EventBus from './EventBus'
+
+    export default {
+        props: {
+            cellData: String,
+            rowIndex: Number,
+            cellIndex: Number,
+        },
+        methods: {
+            onClickTd() {
+                if(this.cellData) return
+                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex) // emit을 이용하여 이벤트 실행
+            }
+        }
+    }
+</script>
